e3c2f5f02735515a0c1fe0d5eecb61de179a113f
Flcable <flcable@gmail.com>
2019-06-14 14:39:07 +0100

    Others entities and urs methods

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -9,5 +9,10 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
     }
 }
