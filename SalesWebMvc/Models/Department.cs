ca98ea9cf6459bac15654a25ce5f6a5b8391f310
Flcable <flcable@gmail.com>
2019-06-14 15:21:47 +0100

    Others entities

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -14,5 +14,22 @@ namespace SalesWebMvc.Models
         public Department()
         {
         }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
+        
     }
 }
